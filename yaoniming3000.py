859422ba806c314cab7b714c11592138e2984b7d
hyfh <zhangleihebut@outlook.com>
2018-04-01 17:26:48 +0800

    vocab review for 3000

--- yaoniming3000.py
+++ yaoniming3000.py
@@ -8,7 +8,8 @@ class Word(object):
     full = attr.ib()
 
 # todo: save to db
-temp = {}
+tempDic = {}
+tempArray = []
 
 def process(src):
     lines = src.splitlines()
@@ -26,8 +27,53 @@ with open('gre3000/source_from_github.txt', encoding='utf8') as f:
     sp = (i.strip().replace('A: ', ' ') for i in c.split('Q:') if i.strip())
     tps = (process(i) for i in sp)
     for i in tps:
-        temp[i.title] = i
+        tempArray.append(i)
+        tempDic[i.title] = i
 
 def search(w):
-    if w in temp:
-        return temp[w]
+    if w in tempDic:
+        return tempDic[w]
+
+if __name__ == '__main__':
+    import sys
+    from colorama import init, Fore, Style
+    init(autoreset=True)
+
+    left = ''
+    right = ''
+    ran = True
+    if len(sys.argv) >= 3:
+        left = sys.argv[1]
+        right = sys.argv[2]
+    else:
+        sys.exit(0)
+    if len(sys.argv) >= 4 and sys.argv[3] == 'r':
+        ran = False
+
+    s = tempArray[tempArray.index(tempDic[left]) : tempArray.index(tempDic[right]) + 1]
+    if ran:
+        import random
+        random.shuffle(s)
+    print('========={}========='.format(len(s)))
+
+    i = 0
+    while True:
+        if i >= len(s):
+            break
+
+        print(str(i) + '. ' + Fore.GREEN + s[i].title, end='')
+        inin = input()
+        if inin == 'q':
+            break
+
+        print('  ' + '\n  '.join(s[i].brief.splitlines()))
+
+        inin = input()
+        if inin == 'q':
+            break
+        if inin:
+            print(Fore.YELLOW + ' ' + s[i].full)
+            print()
+
+        i += 1
+
