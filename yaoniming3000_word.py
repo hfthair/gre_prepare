d380cc6fc3cf8fff7667c7aac55aa995741b3704
hyfh <zhangleihebut@outlook.com>
2018-04-14 16:25:03 +0800

    db & xls

--- yaoniming3000_word.py
+++ yaoniming3000_word.py
@@ -1,9 +1,24 @@
 
 import attr
 
+
+
+
 @attr.s
 class Word(object):
     title = attr.ib()
     brief = attr.ib()
     full = attr.ib()
     position = attr.ib(default=0)
+
+
+def create_word(src):
+    lines = src.splitlines()
+    title, pron = lines[0].split('  ')
+    def get_briefs(line):
+        t = line.split('：')[0].strip()
+        t = t[t.index(' '):].strip()
+        return t
+
+    brief = '\n'.join(get_briefs(i) for i in lines[1:] if i.startswith(' ♠'))
+    return Word(title, brief, src)
