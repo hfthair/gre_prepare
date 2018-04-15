ef9c2c457bc22144a677f323a0c3f31dbb74eaa7
hyfh <zhangleihebut@outlook.com>
2018-04-15 21:55:57 +0800

    analyze longest substr

--- analyze3000.py
+++ analyze3000.py
@@ -1,9 +1,29 @@
-from yaoniming3000 import wordArrayAll, wordByTitle
+from yaoniming3000 import wordByTitle
+from difflib import SequenceMatcher as SM
+from itertools import combinations
 
-rank_syn = []
+def common_substr(a, b):
+    sm = SM(None, a, b)
+    i, _, k = sm.find_longest_match(0, len(a), 0, len(b))
+    if k > 6:
+        return a[i:i+k]
+    return None
 
-def count_syn(w):
+all_words = wordByTitle.keys()
+col = {}
+for i, j in combinations(all_words, 2):
+    c = common_substr(i, j)
+    if c:
+        if c not in col:
+            col[c] = set()
+        col[c].add(i)
+        col[c].add(j)
+
+keys = sorted(col.keys(), key=lambda x: len(x), reverse=True)
+
+
+
+
+if __name__ == '__main__':
     pass
-
-t = ((i, count_syn(i)) for i in wordArrayAll)
-
+    # print(common_substr('xxabxcdyy', 'asabcdykdls'))
