81ba3e16979209d498589f00f97973a6e33b4e2d
hyfh <zhangleihebut@outlook.com>
2018-04-16 00:02:19 +0800

    'analyze3000'

--- analyze3000.py
+++ analyze3000.py
@@ -1,29 +1,39 @@
+import pickle
+import os
 from yaoniming3000 import wordByTitle
 from difflib import SequenceMatcher as SM
 from itertools import combinations
 
+
 def common_substr(a, b):
     sm = SM(None, a, b)
     i, _, k = sm.find_longest_match(0, len(a), 0, len(b))
-    if k > 6:
+    if k > 3:
         return a[i:i+k]
     return None
 
-all_words = wordByTitle.keys()
-col = {}
-for i, j in combinations(all_words, 2):
-    c = common_substr(i, j)
-    if c:
-        if c not in col:
-            col[c] = set()
-        col[c].add(i)
-        col[c].add(j)
+substrs = {}
+keys = []
 
-keys = sorted(col.keys(), key=lambda x: len(x), reverse=True)
+if os.path.exists('gre3000/analyze3000.pickle'):
+    with open('gre3000/analyze3000.pickle', 'rb') as f:
+        substrs, keys = pickle.load(f)
+else:
+    all_words = wordByTitle.keys()
 
+    for i, j in combinations(all_words, 2):
+        c = common_substr(i, j)
+        if c:
+            if c not in substrs:
+                substrs[c] = set()
+            substrs[c].add(i)
+            substrs[c].add(j)
 
+    keys = sorted(substrs.keys(), key=lambda x: len(x), reverse=True)
+    with open('gre3000/analyze3000.pickle', 'wb') as f:
+            pickle.dump((substrs, keys), f)
 
 
 if __name__ == '__main__':
-    pass
-    # print(common_substr('xxabxcdyy', 'asabcdykdls'))
+    # print('\n'.join(substrs[keys[0]]))
+    print('\n'.join(keys))
