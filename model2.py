d2025c752cb04092d209d65812dd5b78712472f6
hyfh <zhangleihebut@outlook.com>
2018-04-30 11:03:35 +0800

    fix review for m2

--- model2.py
+++ model2.py
@@ -103,9 +103,15 @@ if __name__ == '__main__':
         print(Fore.GREEN + t + Fore.RESET + ' | '.join(w.brief.splitlines()), end='')
 
     def get_data_of_day(fr, to):
-        frr = datetime.datetime.combine(fr, datetime.time.min)
-        too = datetime.datetime.combine(fr, datetime.time.max)
-        return Word.select().where(Word.last_modify>frr and Word.last_modify<too)
+        # frr = datetime.datetime.combine(fr, datetime.time.min)
+        # too = datetime.datetime.combine(fr, datetime.time.max)
+        return Word.select().where(~Word.last_modify.is_null()).\
+            where(Word.last_modify.year>=fr.year).\
+            where(Word.last_modify.month>=fr.month).\
+            where(Word.last_modify.day>=to.day).\
+            where(Word.last_modify.year<=to.year).\
+            where(Word.last_modify.month<=to.month).\
+            where(Word.last_modify.day<=to.day)
 
     def print_ws(ws, touch):
         cnt = 0
@@ -123,24 +129,26 @@ if __name__ == '__main__':
         ws = get_data_of_day(datetime.date.today(), datetime.date.today())
 
         if ws.count() > 30:
+            print('==== review today ({}) ===='.format(ws.count()))
             ws = ws.order_by(fn.random())
             print_ws(ws, False)
         else:
-            ws = Word.select().where(Word.last_modify==None).limit(65).\
-                order_by(((Word.count-0.8) * Word.id).desc(), Word.id.desc())
+            print('==== new today ====')
+            ws = Word.select().where(Word.last_modify.is_null()).\
+                order_by(((Word.count-0.8) * Word.id).desc(), Word.id.desc()).\
+                limit(65)
             print_ws(ws, True)
 
 
     def task_for_review():
         cr = (1, 2, 3, 5)
         cr = [datetime.datetime.now().date()-datetime.timedelta(days=i) for i in cr]
-        cr = [(i.year, i.month, i.day) for i in cr]
-        print(cr)
+        cr = [i.year*10000+i.month*10+i.day for i in cr]
 
-        # not work!!!
+        # how does peewee support |(col1, col2) << ((a,b), (c,d))| ?
         ws = Word.select().\
-                where(Word.last_modify!=None and \
-                (Word.last_modify.year, Word.last_modify.month, Word.last_modify.day) in cr).\
+                where(Word.last_modify!=None & \
+                    (Word.last_modify.year*10000+Word.last_modify.month*100+Word.last_modify.day) << cr).\
                 order_by(Word.last_modify.desc(), fn.random())
 
         print_ws(ws, False)
